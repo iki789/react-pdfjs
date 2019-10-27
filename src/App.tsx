c6c89fd59c5a98ad4cf89bfdf19c7acf2e59bbc4
Ikhlak <ikhlak789@gmail.com>
2019-10-28 01:24:35 +0530

    handle CORS

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,28 @@ import React from "react";
 import "./App.css";
 import PDFViewer from "./pdf-viewer/pdf-viewer";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <PDFViewer src="http://localhost:3000/test.pdf"></PDFViewer>
-    </div>
-  );
-};
+export default class App extends React.Component {
+  state = {};
+  pdf: any;
+  pdfUrl: string =
+    "https://www.tdcanadatrust.com/document/PDF/accessibility/tdct-accessibility-3515-privacy-code-lp.pdf";
 
-export default App;
+  componentDidMount() {
+    fetch("https://cors-anywhere.herokuapp.com/" + this.pdfUrl, {
+      headers: {
+        "content-type": "application/pdf"
+      },
+      credentials: "omit",
+      mode: "cors"
+    })
+      .then(res => res.arrayBuffer())
+      .then(buff => {
+        this.pdf = <PDFViewer src={buff} keywords="privacy policy"></PDFViewer>;
+        this.setState({ ...this.state });
+      });
+  }
+
+  render() {
+    return <div className="App">{this.pdf}</div>;
+  }
+}
