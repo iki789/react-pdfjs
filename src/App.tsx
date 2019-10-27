1abf45475f5ec0d3ee534a400d7c4ed262109c8c
Ikhlak <ikhlak789@gmail.com>
2019-10-28 01:31:28 +0530

    added searhKeywords variable

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import PDFViewer from "./pdf-viewer/pdf-viewer";
 export default class App extends React.Component {
   state = {};
   pdf: any;
+  searchKeywords: string = "privacy policy";
   pdfUrl: string =
     "https://www.tdcanadatrust.com/document/PDF/accessibility/tdct-accessibility-3515-privacy-code-lp.pdf";
 
@@ -18,7 +19,9 @@ export default class App extends React.Component {
     })
       .then(res => res.arrayBuffer())
       .then(buff => {
-        this.pdf = <PDFViewer src={buff} keywords="privacy policy"></PDFViewer>;
+        this.pdf = (
+          <PDFViewer src={buff} keywords={this.searchKeywords}></PDFViewer>
+        );
         this.setState({ ...this.state });
       });
   }
